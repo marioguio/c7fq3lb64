5047e9bc1d66faaeeac8ac22346e903a470a5f69
marioguio <mario_guio@outlook.com>
2017-08-28 11:57:45 -0500

    aplicando los cambios para el reto

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,13 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
-      @expenses = Expense.order("date DESC")
-      if params[:concept].present?
-        @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
-      end
-      if params[:category_id].present?
-        @expenses = @expenses.where("category_id = ?", params[:category_id])
-      end
+    @expenses = Expense.order("date DESC")
+    @expenses = @expenses.where("user_id = ?", current_user)
+    if params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
     end
+    if params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    end
+  end
 end
